c04e548edb8a3c656a2948b08e978dc40d4ea0e3
Niha-21 <nihakamalnk@gmail.com>
2026-01-17 14:47:49 +0530

    Added constructor in Book entity

--- book-service/src/main/java/com/nksbookstore/book/entity/Book.java
+++ book-service/src/main/java/com/nksbookstore/book/entity/Book.java
@@ -10,11 +10,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "books")
 @AllArgsConstructor
+@NoArgsConstructor
 public class Book {
     
     @Id
