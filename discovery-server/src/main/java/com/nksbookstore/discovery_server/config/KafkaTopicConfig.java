d09f2e7be53a0dff9b63acb9229116ce9023dad4
Niha-21 <nihakamalnk@gmail.com>
2026-01-18 02:45:58 +0530

    Updated kafka config

--- discovery-server/src/main/java/com/nksbookstore/discovery_server/config/KafkaTopicConfig.java
+++ discovery-server/src/main/java/com/nksbookstore/discovery_server/config/KafkaTopicConfig.java
@@ -8,22 +8,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class KafkaTopicConfig {
 
-    @Value("${kafka.topics.clear-cart.name}")
+    @Value("${spring.kafka.topics.clear-cart.name}")
     private String clearCartTopic;
 
-    @Value("${kafka.topics.clear-cart.partitions}")
+    @Value("${spring.kafka.topics.clear-cart.partitions}")
     private int clearCartPartitions;
 
-    @Value("${kafka.topics.clear-cart.replication-factor}")
+    @Value("${spring.kafka.topics.clear-cart.replication-factor}")
     private short clearCartReplicationFactor;
 
-    @Value("${kafka.topics.clear-cart-dlt.name}")
+    @Value("${spring.kafka.topics.clear-cart-dlt.name}")
     private String clearCartDltTopic;
 
-    @Value("${kafka.topics.clear-cart-dlt.partitions}")
+    @Value("${spring.kafka.topics.clear-cart-dlt.partitions}")
     private int clearCartDltPartitions;
 
-    @Value("${kafka.topics.clear-cart-dlt.replication-factor}")
+    @Value("${spring.kafka.topics.clear-cart-dlt.replication-factor}")
     private short clearCartDltReplicationFactor;
 
     @Bean
